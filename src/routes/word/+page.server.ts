b14acffa3e65c7242402a65dbe88e9baa0f1112a
vincent-wijaya <vincentwijaya2001@yahoo.com>
2024-06-16 01:13:45 +1000

    Old changes

--- src/routes/word/+page.server.ts
+++ src/routes/word/+page.server.ts
@@ -3,8 +3,9 @@ import { error, redirect } from '@sveltejs/kit'
 import { supabase } from '$lib/supabaseClient.js'
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load( table: string ) {
-    const { data, error } = await supabase.from(table).select('*')
+export async function load({ params }) {
+    const { tableName } = params
+    const { data, error } = await supabase.from(tableName).select('*')
     if (error) {
         throw new Error(error.message)
     }
